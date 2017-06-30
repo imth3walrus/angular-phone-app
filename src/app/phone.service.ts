a34713cdf7ab790a258cf9d4152d5ede3cf07f2c
Juan Duque <sebastian@byafro.com>
2017-06-29 21:28:28 -0400

    working

--- src/app/phone.service.ts
+++ src/app/phone.service.ts
@@ -9,19 +9,19 @@ export class PhoneService {
   constructor(private myHttp: Http) { }
 
   getList() {
-    return this.myHttp.get('${this.BASE_URL}/api/phones')
+    return this.myHttp.get(`${this.BASE_URL}/api/phones`)
     .toPromise()
     .then(apiResponse => apiResponse.json())
   }
 
   get(id) {
-    return this.myHttp.get('${this.BASE_URL}/api/phones/${id}')
+    return this.myHttp.get(`${this.BASE_URL}/api/phones/${id}`)
     .toPromise()
     .then(apiResponse => apiResponse.json())
   }
 
   remove(id) {
-    return this.myHttp.delete('${this.BASE_URL}/api/phones/${id}')
+    return this.myHttp.delete(`${this.BASE_URL}/api/phones/${id}`)
     .toPromise()
     .then(apiResponse => apiResponse.json())
   }
